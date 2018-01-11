9359e6aedd7c29864ff7a5477ddf9fa9f06efad9
youngsamwei <w3wei@sina.com>
2018-01-11 19:44:29 +0800

    模块化index.html

---
+++ src/main/webapp/js/knowledgepoint_load.js
@@ -0,0 +1,107 @@
+
+function loadKnowledgepointParagraph (){
+
+        var qname  = getQueryString("qname");
+        var qid = getQueryString("qid");
+        var qstr = "";
+        if ((qid != '') && (qid != null)){
+          qstr = "id=" + qid;
+         }else if ((qname != '') && (qname != null)){
+          qstr = "knowledgepointName=" + qname;
+         }else{
+          /*出错处理*/
+         }
+         qstr = encodeURI(qstr);
+        $.ajax({
+            type: "POST",
+            url: "/knowledgepoint/list.do?"+qstr,
+            contentType: "application/json; charset=utf-8",
+            data: "{}",
+            dataType: "json",
+            success: function (result) {
+                displayTitle(result);
+                displayDescBlocks(result);
+                /*setEditor();*/
+            },
+            "error": function (result) {
+                var response = result.responseText;
+                alert('errot');
+            }
+        });
+}
+
+
+    function getQueryString(name) {
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+        var r = window.location.search.substr(1).match(reg);
+        if (r != null) return decodeURI(r[2]); return null;
+    }
+
+    /*获取并显示知识点的名字*/
+    function displayTitle(result ){
+          var knowledgepointId = result[0].id;
+                var knowledgepointName = result[0].knowledgepointName;
+                var button0 = "<a href='javascript:openKnowledgepointAddDialog(0)' class='left_button top_button fa fa-plus-square-o' title='增加知识点'> 增加知识点</a> ";
+                /*TODO:在所有段落的最后增加，需要特殊处理该段落的顺序编号*/
+                var button1 = "<a href='javascript:openArticleAddDialog(" + knowledgepointId + ", 0)' class='middle_button top_button fa fa-plus' title='在最前增加一个段落'> 增加段落</a> ";
+                var button3 = "<a href='javascript:openSearchDialog()' class='middle_button top_button fa fa-search' title='查询'> 查询</a> ";
+                var button2 = "<a href='javascript:void(0)' class='right_button top_button fa fa-star-o' title='收藏，收藏一下'> 收藏</a>";
+
+                var itemHTML = ["<P ><div style='background: #F0F8FF'>",
+                    button2, button1, button3, button0,
+                    "<div  id='content_" + knowledgepointId + "'>",
+                   '<h1>',  knowledgepointName,'</h1>',
+                    "</div>",
+                    "</div></P>"].join('\n');
+                //console.log(itemHTML);
+                $(".kbknowledgepoint").append(itemHTML);
+
+    }
+    /*显示文本块，并在文本块的右上角显示删除，编辑，纠错按钮*/
+    function displayDescBlocks(result) {
+
+            var knowledgepointId = result[0].id;
+            $.ajax({
+                type: "POST",
+                url: "/paragraph/list.do?knowledgepointId=" + knowledgepointId,
+                contentType: "application/json; charset=utf-8",
+                data: "{}",
+                dataType: "json",
+                success: function (paragraphs) {
+                    $.each(paragraphs, function (index, element) {
+                        var content  = (( element.paragraphContent == "")||( element.paragraphContent == null))? '<P>&nbsp;</P>'
+                          : htmlDecode(element.paragraphContent);
+                        var paragraphOrder = element.paragraphOrder + 1;
+                        var button1 = "<a href='javascript:openArticleModifyDialog(\"" + element.id + "\",\"content_" + element.id + "\");' class='right_button fa fa-edit' title='编辑，贡献一下'> 编辑</a>";
+                        var button2 = "<a href='javascript:void(0)' class='middle_button fa fa-trash-o' title='删除此段落'> 删除</a> ";
+                        var button3 = "<a href='javascript:void(0)' class='left_button fa fa-bug' title='纠错，较真一下'> 纠错</a>";
+                        var button4 = "<a href='javascript:openArticleAddDialog(" + knowledgepointId +", " + paragraphOrder + ")' class='middle_button fa fa-plus' title='在此段落后增加'> 增加</a> ";
+
+                        var itemHTML = ["<P ><div style='background: #F0F8FF'>",
+                            button1, button4, button2, button3,
+                            "<div class='content' id='content_" + element.id + "'>",
+                            content,
+                            "</div>",
+                            "</div></P>"].join('\n');
+                        //console.log(itemHTML);
+                        $(".kbparagraph").append(itemHTML);
+                    })
+                },
+                "error": function (result) {
+                    var response = result.responseText;
+                    alert('errot');
+                }
+
+            });
+
+    }
+
+
+//Html编码获取Html转义实体
+function htmlEncode(value){
+ return $('<div/>').text(value).html();
+}
+//Html解码获取Html实体
+function htmlDecode(value){
+ return $('<div/>').html(value).text();
+}
---
+++ src/main/webapp/js/knowledgepoint_manager.js
@@ -0,0 +1,147 @@
+    var url;
+
+    /* 与index.html配合 */
+    function setEditor(){
+
+        $('.content').click(function(e){
+            var $target = $(this);
+            var content = $target.html();
+            openArticleModifyDialog('0',content);
+        })
+    }
+
+    function resetEditor(editorID) {
+        return UE.getEditor(editorID, {
+            initialFrameHeight: '100%',
+            initialFrameWidth: '100%',
+            enableAutoSave: false,
+            elementPathEnabled: false,
+            wordCount: false
+            /*,  toolbars: [
+             [
+             'fontfamily', 'fontsize', 'forecolor', 'backcolor', 'bold', 'italic', 'underline', '|',
+             'link', '|',
+             ]
+             ]  */
+        });
+    }
+
+    function openArticleModifyDialog(id, contentid) {
+
+        $("#dlg_edit_paragraph").dialog("open").dialog("setTitle", "编辑段落");
+        $('#dlg_edit_paragraph').window('center');
+
+        var html = '<div id="myEditor" name="paragraphContent"></div>';
+        $('#editor').append(html);
+
+        var ue =  resetEditor("myEditor");
+
+        var $target = $('#' + contentid);
+        var content = $target.html();
+        ue.ready(function(){
+            ue.setContent(content);
+            ue.focus();
+        });
+        url = "/paragraph/edit.do?id="  + id;
+
+    }
+    function closeArticleDialog() {
+        $("#dlg_edit_paragraph").dialog("close");
+    }
+
+
+    function deleteArticle(paragraphId) {
+
+        $.messager
+                .confirm(
+                        "系统提示",
+                        "您确认要删除这<font color=red>" + paragraphId
+                        + "</font>吗？",
+                        function (r) {
+                            if (r) {
+                                $
+                                        .post(
+                                                "/paragraph/delete.do",
+                                                {
+                                                    id: paragraphId
+                                                },
+                                                function (result) {
+                                                    if (result.success) {
+                                                        $.messager.alert(
+                                                                "系统提示",
+                                                                "数据已成功删除！");
+
+                                                    } else {
+                                                        $.messager.alert(
+                                                                "系统提示",
+                                                                "数据删除失败！");
+                                                    }
+                                                }, "json");
+                            }
+                        });
+
+    }
+
+    function openKnowledgepointAddDialog(){
+            $("#dlg_add_knowledgepoint").dialog("open").dialog("setTitle", "请输入知识点名称");
+            $('#input_knowledgepoint').focus();
+    }
+
+    function closeKnowledgepointAddDialog(){
+            $("#dlg_add_knowledgepoint").dialog("close");
+    }
+
+    function addKnowledgepoint(){
+        var knowledgepointName = $('#input_knowledgepoint').val();
+        $.ajax({
+            type: "POST",
+            url: "/knowledgepoint/add.do?knowledgepointName=" + knowledgepointName,
+            contentType: "application/json; charset=utf-8",
+            data: "{}",
+            dataType: "json",
+            success: function (result) {
+               closeKnowledgepointAddDialog();
+               window.location.href='index.html?qname='+knowledgepointName;
+            },
+            "error": function (result) {
+                var response = result.responseText;
+                alert('errot');
+            }
+
+        });
+    }
+
+    function openArticleAddDialog(knowledgepointId, paragraphOrder) {
+        var html = '<div id="myEditor" style="width:100%; height:100%;" name="paragraphContent"></div>';
+        $('#editor').append(html);
+
+        var ue =  resetEditor("myEditor");
+        ue.ready(function(){
+            ue.setContent("");
+            ue.focus();
+        });
+        $("#dlg_edit_paragraph").dialog("open").dialog("setTitle", "添加段落");
+        url = "/paragraph/add.do?knowledgepointId=" + knowledgepointId + "&paragraphOrder=" + paragraphOrder;
+    }
+
+    function saveArticle() {
+        $("#fm").form("submit", {
+            url: url,
+            onSubmit: function () {
+                return $(this).form("validate");
+            },
+            success: function (result) {
+
+                $("#dlg_edit_paragraph").dialog("close");
+                location.reload() ;
+            }
+        });
+    }
+
+    function openSearchDialog(){
+        $("#dlg_search").dialog("open").dialog("setTitle", "查询");
+    }
+
+    function closeSearchDialog() {
+        $("#dlg_search").dialog("close");
+    }
