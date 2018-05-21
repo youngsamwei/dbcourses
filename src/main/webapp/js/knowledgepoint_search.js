06cec8bc5ac34ff23552196c9b5cb48b95e684dd
yanxiangjiang <52265729@qq.com>
2018-05-21 10:31:46 +0800

    知识点的增加、删除、修改，特性：优化手机端输入框显示页面； 增加时检测是否存在重名知识点， 如果存在则不增加，直接跳转至该知识点。 知识点的查询：微信端，网页端。特性：查询时不区分大小写； 显示10条结果（显示结果条数可以配置）； 每项知识点的第一个段落的文字作为描述, 如果多页则需要分页显示

--- src/main/webapp/js/knowledgepoint_search.js
+++ src/main/webapp/js/knowledgepoint_search.js
@@ -7,32 +7,59 @@
     function closeSearchDialog() {
         $("#dlg_search").dialog("close");
     }
-
+    //知识点搜索分页上一页
+    function shangyiye(){
+        var value = $('#ssvalue').val();
+       var name = $('#box').val();
+        var page = $('#pageaa').val()*1-1*1;
+        // alert(name+";"+page);
+        doSearchKnowledge(value,name,page);
+    }
+    //知识点搜索分页下一页
+    function xiayiye(){
+        var value = $('#ssvalue').val();
+       var name = $('#box').val();
+        var page = $('#pageaa').val()*1+1*1;
+        // alert(name+";"+page);
+        doSearchKnowledge(value,name,page);
+    }
 /*将查询结果显示在div:search_result 中
 TODO: 需要支持分页，显示所有条数。
 TODO：需要controller，service，dao的支持，把知识点和（第一个段落的前N个字）一起返回。
-TODO: 支持输入时自动提示，每输入一个字或词，触发检索事件并显示出。
+每项知识点的第一个段落的文字作为描述
 */
-    function doSearchKnowledge(value, name){
+   //这个函数重新加入了页码
+    function doSearchKnowledge(value, name,page){
         $.ajax({
             type: "POST",
-            url: "/knowledgepoint/list.do?knowledgepointName="+value,
+            url: "/knowledgepoint/list.do?knowledgepointName="+value+"&page="+page,
             contentType: "application/json; charset=utf-8",
             data: "{}",
             dataType: "json",
             success: function (result) {
                 var kbhtml = "<div id='kp_inner'>";
                 $.each(result, function (index, element) {
-                    kbhtml += "<div><a href='javascript:loadKnowledgepointParagraph(" + element.id + ")'>" + element.knowledgepointName + "</a></div>";
+                    kbhtml += "<div><a href='javascript:loadKnowledgepointParagraph(" + element.id + ")'>"+element.knowledgepointName+"<input type='hidden' id='pageaa'value='"+element.page+"'>"+"<input type='hidden' id='ssvalue'value='"+value+"'>"+"</a>";
+                    $.each(element.paragraphlist,function(i,e){
+                        if(i==0){
+                            kbhtml += htmlDecode(e.paragraphContent);
+                        }
+                    })
+
+                    kbhtml += "</div>";
                 })
+
                 kbhtml += "</div>";
+                var  a = "<a id='btn' href='#' class='easyui-linkbutton'>"+"easyui"+"</a>";
+                //把html标签去掉
+                kbhtml += htmlDecode()
                 $("#kp_inner").remove();
                 $('#search_result').append(kbhtml);
 
             },
             "error": function (result) {
                 var response = result.responseText;
-                alert('errot');
+                alert('error');
             }
         });
     }
